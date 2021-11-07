1004c6bf704a63dbc4a8a5f7d7ea2b7a367048e9
ichyryk <irachyryk2@gmail.com>
2021-11-07 20:44:21 +0200

    add verififcation

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,12 +1,15 @@
-
 const signup  = require('./signup')
 const login = require('./login')
 const logout  = require('./logout')
-const  current  = require('./current')
+const current = require('./current')
+const verify = require('./verify')
+const repeatEmailVerification = require('./repeatEmailVerification')
 
 module.exports = {
   signup,
   login,
   logout,
-  current
+    current,
+    verify,
+  
 }
