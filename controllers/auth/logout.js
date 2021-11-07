4945299fa28339e66a95c0afe0ceea7d4610e20b
ichyryk <irachyryk2@gmail.com>
2021-11-07 20:37:39 +0200

    add logout

---
+++ controllers/auth/logout.js
@@ -0,0 +1,11 @@
+const { User } = require('../../models')
+const sendSuccessResponse = require('../../helpers')
+
+const logout = async(req, res) => {
+  const { _id } = req.user
+  await User.findByIdAndUpdate(_id, { token: null })
+  sendSuccessResponse(res, 'Success logout', 200)
+}
+
+module.exports = 
+  logout
