562e6c644b5f13588c91a295cc53a111703d6b4f
ichyryk <irachyryk2@gmail.com>
2021-11-09 11:01:29 +0200

    add setBalance

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -3,7 +3,9 @@ const { User } = require('../../models')
 
 const logout = async (req, res) => {
   const { _id } = req.user
+
   await User.findByIdAndUpdate(_id, { token: null })
+
   res.json({
     status: 'success',
     code: 200,
---
+++ controllers/auth/setBalance.js
@@ -0,0 +1,16 @@
+const { User } = require('../../models')
+const { sendSuccessResponse } = require('../../helpers')
+
+const setBalance = async (req, res) => {
+  const { _id } = req.user
+  const { balance } = req.body
+
+  const newBalance = await User.findByIdAndUpdate(
+    _id,
+    { balance },
+    { new: true })
+
+  sendSuccessResponse(res, { newBalance }, 201)
+}
+
+module.exports = setBalance
