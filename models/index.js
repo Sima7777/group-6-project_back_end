6b7a35eab3ccd1250b060a570492833917265e18
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2021-11-09 19:03:02 +0200

    fix

--- models/index.js
+++ models/index.js
@@ -3,9 +3,5 @@ const { Transaction } = require('./transaction')
 
 module.exports = {
   User,
-<<<<<<< HEAD
   Transaction
-=======
-  Transaction,
->>>>>>> effac5ff5ed44972fe877aeffc69cffc2482b715
 }
