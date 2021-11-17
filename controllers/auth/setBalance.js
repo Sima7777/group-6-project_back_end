44a4a7ee96a5580daa3753fa1568c075c8a9abdb
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2021-11-17 02:16:17 +0200

    fix balance

--- controllers/auth/setBalance.js
+++ controllers/auth/setBalance.js
@@ -1,20 +1,26 @@
 const { User } = require('../../models')
-// const { sendSuccessResponse } = require('../../helpers')
+const { NotFound } = require('http-errors')
 
-const setBalance = async (req, res) => {
-  const { _id } = req.user
-  const { balance } = req.body
-
-  const newBalance = await User.findByIdAndUpdate(
-    _id,
-    { balance },
-    { new: true }
-  )
-
-  // res.json(res, { newBalance: balance }, 201)
-  res.status(201).json({
-    balance: newBalance.balance
-  })
+const setBalance = async (req, res, next) => {
+  try {
+    const { _id } = req.user._id
+    const { balance } = req.body
+    const updateUser = await User.findByIdAndUpdate(
+      _id,
+      { balance },
+      { new: true }
+    )
+    if (!updateUser) {
+      throw new NotFound()
+    }
+    res.json({
+      user: {
+        email: updateUser.email,
+        balance: updateUser.balance,
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
 }
-
 module.exports = setBalance
