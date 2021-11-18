b9cb189c0d4568b030d935b81839c7f96bf1c205
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-11-18 22:47:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+  )
+  next()
+})
 app.use(express.json())
 app.use(express.static('public'))
 
