363371721e958ae2a175013ba4291e0a5f68d640
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2021-11-19 15:53:07 +0200

    fix income,expence

--- controllers/transactions/addIncome.js
+++ controllers/transactions/addIncome.js
@@ -1,23 +1,27 @@
 const { Transaction } = require('../../models/transaction')
 const { User } = require('../../models/user')
+const sendSuccessResponse = require('../../helpers/sendSuccessResponse')
 
 const addIncome = async (req, res, next) => {
+  const { amount } = req.body
+  const { _id } = req.user
+  const user = await User.findById(_id)
+  const newTransaction = {
+    ...req.body,
+    isIncome: true,
+    owner: _id,
+  }
   try {
-    const { amount } = req.body
-    const userId = req.user._id.toString()
-    const transactionData = await Transaction.create({
-      ...req.body,
-      isIncome: true,
-      owner: userId,
-    })
-    res.status(201).json({
-      transactionData
-    })
-    const user = await User.findById(userId)
-    user.balance += amount
-    await user.save()
+    const newBalance = user.balance + amount
+    await User.findByIdAndUpdate(
+      _id,
+      { balance: newBalance },
+      { new: true },
+    )
+    const result = await Transaction.create(newTransaction)
+    sendSuccessResponse(res, { result }, 201)
   } catch (error) {
-    next(error)
+    res.status(400).json(error.message)
   }
 }
 
